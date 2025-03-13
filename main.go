c20ef123742dd3c49482c2771973640ba4f0d332
KleitonBarone <25616578+KleitonBarone@users.noreply.github.com>
2025-03-13 19:19:33 -0300

    feat: add database connection

--- main.go
+++ main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/KleitonBarone/chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	dbQueries      *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+
+	dbURL := os.Getenv("DB_URL")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbQueries := database.New(db)
+
 	const port = "8080"
 	const staticDir = "."
 
@@ -20,6 +38,7 @@ func main() {
 
 	apiCfg := &apiConfig{
 		fileserverHits: atomic.Int32{},
+		dbQueries:      dbQueries,
 	}
 
 	staticFiles := http.FileServer(http.Dir(staticDir))
